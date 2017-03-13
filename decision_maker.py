804960603e2bb958dab654803491cf4e6b868eff
rhagan21 <r713h041@ku.edu>
2017-03-13 11:22:34 -0500

    adding shutdown and main state enums

--- decision_maker.py
+++ decision_maker.py
@@ -13,6 +13,9 @@ from autoscripts import takeoff
 from autoscripts import land
 import serial
 
+def enum(**enums):
+    return type('Enum', (), enums)
+
 mw = MultiWii('/dev/ttyUSB0') # pass in the port to create a new multiwii object
 ser=serial.Serial()
 
@@ -20,25 +23,20 @@ ser=serial.Serial()
 # IMMEDIATE GOAL: Startup scripts and X/Y adjustments
 # QUESTION: What does "Naze sensors good" mean?
 # Call with python2.7 (on rachel's computer: use python2 in /usr/bin/python2)
+# Need to research how to create buzzes. Alternatively the Naze may automatically make different noises as it enters different modes such as its waking up sound
 
 # These vars haven't been set yet but they will be used to tell the drone what state to go into
-#shutdown_state:
-#define ACTIVE = 1
-#define PREPARE_TO_SHUTDOWN = 2
-#define SHUTDOWN = 3
+shutdown_state = enum(ACTIVE=1, PREPARE_TO_SHUTDOWN=2, SHUTDOWN=3)
 
 #main_state:
-#define PREFLIGHT_CHECKS = 1
-#define TAKEOFF = 2
-#define TRACKING_TARGET = 3
-#define RELOCATING_TARGET = 4
+main_state = enum(PREFLIGHT_CHECKS=1, TAKEOFF=2, TRACKING_TARGET=3, RELOCATING_TARGET=4)
 
 ################
 # PRE TAKE OFF #
 ################
 
-#shutdown_state = 1
-#main_state = 1
+shutdown_state = shutdown_state.ACTIVE
+main_state = main_state.PREFLIGHT_CHECKS
 
 #IGNORE FOR NOW:
 # Is battery low?
@@ -53,7 +51,7 @@ roll = gyro_signal[0]
 pitch = gyro_signal[1]
 
 print("Roll: ", roll, "Pitch: ", pitch)
-# TODO: Using arbitrary values
+# TODO: Using arbitrary values, need to test to see what boundaries are acceptable
 while( roll < 1000 or roll > 2000 or pitch < 1000 or pitch > 2000 ):
     print("Gyro returned a bad roll (", roll, ") or pitch (", pitch, "), Give bad status buzz, wait 3 seconds and shut off")
     # give "bad status" buzz
@@ -66,6 +64,7 @@ timeout = time.time() + 6
 
 #Takeoff procedure
 print("Enter takeoff procedure")
+main_state = main_state.TAKEOFF
 # Set to hover mode (Use the takeoff(mw) function in autoscripts.py)
 # takeoff(self)
 # go straight up
@@ -78,30 +77,42 @@ print("Enter takeoff procedure")
 #off_signal = 1
 # Off signal detected
 #if(off_signal):
-    # Yes: Shut down (autoscripts.land(mw))
-    #print("Off signal detected, shut down")
-    # No: Continue
+    # print("Off signal detected, shut down")
+    # main_state = main_state.PREPARE_TO_SHUTDOWN
+    # main_state = main_state.SHUTDOWN
+    # autoscripts.land(mw)
 
+#IGNORE FOR NOW
 # Battery low?
 #low_battery_signal = 1
 #if(low_battery_signal):
-    # Yes: Low volt buzz, flash red light, shut down (autoscripts.land(mw))
-    #print("Low battery detected, give low volt buzz, flash red light, and shut down")
-    # No: Continue
+    # Yes: Low volt buzz, flash red light
+    # print("Low battery detected, give low volt buzz, flash red light, and shut down")
+    # main_state = main_state.PREPARE_TO_SHUTDOWN
+    # main_state = main_state.SHUTDOWN
+    # autoscripts.land(mw)
 
-#right_side_up = 1
-#naze_sensors_good = 1
-# Right side up, Naze sensors good?
-    # No: "Bad status" buzz, shut down (autoscripts.land(mw))
-    # Yes: Continue
+# Assuming checking roll & pitch will be sufficient to know if the drone is right side up, we can use the same code from earlier
+gyro_signal = mw.askRC()
+roll = gyro_signal[0]
+pitch = gyro_signal[1]
+# TODO: Any other Naze sensors to check here? Need to calibrate boundaries
+print("Roll: ", roll, "Pitch: ", pitch)
+if( roll < 1000 or roll > 2000 or pitch < 1000 or pitch > 2000 ):
+    print("Drone is not right side up, powering down")
+    # main_state = main_state.PREPARE_TO_SHUTDOWN
+    # main_state = main_state.SHUTDOWN
+    # autoscripts.land(mw)
 
 #have_target = 1
 #position = 1
 #if(!(have target && position == true)):
 # Have target and position == true?
-    #print("Do not have target or position != true; enter relocation loop")
+    # print("Do not have target or position != true; enter relocation loop")
     # No: Relocation loop
         # Can't find: Shut down (autoscripts.land(mw))
+        # main_state = main_state.PREPARE_TO_SHUTDOWN
+        # main_state = 
         # If found it, return to have target and position == true
    # If yes, continue
 
