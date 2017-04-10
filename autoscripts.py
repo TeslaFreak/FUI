ca49414f1b4bdd3818142aca53a5c704987f039c
Chris A <chrisa.freepress@gmail.com>
2017-04-10 11:02:30 -0700

    Update autoscripts.py

--- autoscripts.py
+++ autoscripts.py
@@ -12,23 +12,40 @@ import time
 #Return: NA.
 #Function: Runs script to tell multiwii to takeoff and begin to hover.
 def takeoff(mw):
-	data = [1500, 1500, 1500, 1250, 1000, 0, 0 ,0] #set all values to balanced, mode to angle, and throttle to spinning but no lifting off the ground.
+	print 'beginning takeoff sequence'
+	throttle = 1250
+	data = [1500, 1500, 1500, throttle, 1000, 0, 0 ,0] #set all values to balanced, mode to angle, and throttle to spinning but no lifting off the ground.
+	mw.getData(mw.RAW_IMU)
+	floorMagZ = mw.rawIMU['mz']
+	droneMagZ = floorMagZ
+	print('floor:', floorMagZ, ' drone:', droneMagZ)
+	desiredHeight = 50 #about a foot off the ground is 50 of this arbitrary unit
 	mw.sendCMD(16, mw.SET_RAW_RC, data)
-	timeout = time.time() + 2
-	while True:
+	timeout = time.time() + 8
+	while droneMagZ > floorMagZ - desiredHeight:
 		if time.time() > timeout:
+			print 'timeout reached'
 			break
-		mw.setThrottle(1500)
-		mw.getData(mw.RC)
+		throttle = throttle + 50
+		if throttle > 1850:
+			throttle = 1850
+		mw.setThrottle(throttle)
+		mw.getData(mw.RAW_IMU)
+		droneMagZ = mw.rawIMU['mz']
+		print('drone magz' ,droneMagZ, ' throttle:', throttle)
+		time.sleep(0.5)
 		
 	mw.setAux(1,2000) #turn baro mode on to begin autohovering at current height.
+	mw.setThrottle(1500)
 	mw.getData(mw.RC)
+	print 'takeoff sequence complete'
 		  
 		  
 #Arg: instance of multiwii object.
 #Return: NA.
 #Function: Runs script to tell multiwii to stop and safely land on the ground.
 def land(mw):
+	print 'beginning landing sequence'
 	mw.getData(mw.RC)
 	throttle = mw.rcChannels['throttle'] #get current throttle
 	data = [1500, 1500, 1500, throttle, 1000, 0, 0 ,0] #set all values to balanced, turn off baro mode, and throttle to stable hover.
@@ -39,7 +56,8 @@ def land(mw):
 			throttle = 1000
 		mw.setThrottle(throttle)
 		mw.getData(mw.RC)
-		time.sleep(0.5)
+		time.sleep(1)
+	print 'landing sequence complete'
 		
 	
     
